122042b13f31655aa6d7be1a5c49f4c0c34fee1a
alessandrosampognaro <alsampognaro@gmail.com>
2025-11-28 01:36:58 +0100

    Refactor package name and update instrumented test
    - Rename `ExampleInstrumentedTest.kt` location to `com.expense.management` package path.
    - Update package declaration and expected package name assertion in `ExampleInstrumentedTest`.

--- app/src/androidTest/java/com/expense/management/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/expense/management/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.alessandrogregorio.gestorespese
+package com.expense.management
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.alessandrogregorio.gestorespese", appContext.packageName)
+        assertEquals("com.expense.management", appContext.packageName)
     }
 }
